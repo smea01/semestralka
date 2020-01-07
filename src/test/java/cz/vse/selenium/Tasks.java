75741a267084d5ba9ab5e20db4d050c55a3c4f1d
Anicka <smea01@vse.cz>
2020-01-07 19:20:33 +0100

    vstupné data noveho tasku

--- src/test/java/cz/vse/selenium/Tasks.java
+++ src/test/java/cz/vse/selenium/Tasks.java
@@ -53,7 +53,23 @@ public class Tasks {
         WebDriverWait wait = new WebDriverWait(driver, 3);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("fields_168")));
 
-        
+        //vstup: nazov, typ, status, priorita, popis
+        WebElement searchInput = driver.findElement(By.id("fields_168"));
+        searchInput.sendKeys("smea01_task");
+        driver.findElement(By.id("fields_167"));
+        Select select = new Select(driver.findElement(By.id("fields_167")));
+        select.selectByIndex(1);
+        driver.findElement(By.id("fields_169"));
+        select = new Select(driver.findElement(By.id("fields_169")));
+        select.selectByIndex(0);
+        driver.findElement(By.id("fields_170"));
+        select = new Select(driver.findElement(By.id("fields_170")));
+        select.selectByIndex(2);
+        driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
+        driver.findElement(By.tagName("body")).sendKeys("Popis");
+        driver.switchTo().defaultContent();
+        driver.findElement(By.className("btn-primary-modal-action")).click();
+
         }
 
 }
